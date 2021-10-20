f805473c9139fd13db7e8fdc55f6594f84075846
maryamnal <mariamnalwoga2000@gmail.com>
2021-10-20 13:26:19 +0300

    assignment2

---
+++ practice.py
@@ -0,0 +1,35 @@
+print("i love css2021,".upper())#This is formatting code
+
+print("i love css2021 (rjust 20)".rjust (20))# The second letter is formatted to uppercase
+
+print("i love css2021(capitalize)".capitalize())# changing statment into uppercase for the first letter
+
+print("i like"+ str( " cs_class_code ") + ( " alot/"))
+
+
+print(f'{print}(print function)')
+
+print(f'{type(229)}(print type)')#printing the data type
+
+#list(lists are mutable)
+list_1 = ['one','two','three']
+
+list_1.append('four')
+
+list_1.insert(0,"zero")
+
+#list 2
+list_2 = [1,2,3] #creating list
+
+list_1.extend(list_2)# this is to add list_2 to list_1
+
+print(list_1)
+
+#Dictionary
+family= {'Mariam','Isaac','Ryan','Jemamima','Alison'}
+
+
+
+
+
+
